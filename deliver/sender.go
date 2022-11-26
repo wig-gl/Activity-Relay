2cd88d60cd144395a367b21d020aa9b57bb24803
Rob Thomas <xrobau@gmail.com>
2022-11-26 23:55:12 +0000

    Look at me learning to read golang stack traces!

--- deliver/sender.go
+++ deliver/sender.go
@@ -68,13 +68,14 @@ func sendActivity(inboxURL string, KeyID string, body []byte, privateKey *rsa.Pr
 	starttime := time.Now()
 	resp, err := HttpClient.Do(req)
 	spost["elapsed"] = time.Since(starttime).Milliseconds()
-	spost["status"] = resp.Status
 	if err != nil {
 		spost["clienterr"] = err.Error()
 		publishSend(spost, true)
 		return err
 	}
 	defer resp.Body.Close()
+
+	spost["status"] = resp.Status
 
 	respbody, readerr := io.ReadAll(resp.Body)
 	spost["respbody"] = respbody
